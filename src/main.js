2fce51cd433123f993f61593f7ee1e8c2cb15a39
antonaksyuk <87242472+antonaksyuk@users.noreply.github.com>
2023-11-20 18:53:42 +0100

    commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-// Add your JavaScript here
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const operatorlChars = ["%", "*", "/", "-", "+", "="];
+let outcome = "";
+
+const count = (btnValue) => {
+  if (btnValue === "=" && outcome !== "") {
+    outcome = eval(outcome.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    outcome = "";
+  } else if (btnValue === "DEL") {
+    outcome = outcome.toString().slice(0, -1);
+  } else {
+    if (outcome === "" && operatorlChars.includes(btnValue)) return;
+    outcome += btnValue;
+  }
+  display.value = outcome;
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => count(e.target.dataset.value));
+});
